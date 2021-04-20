277cd5b44112186b351a3530943510e2b6be630d
neogis <sravantesh.neogi@gmail.com>
2021-04-20 19:57:36 +0530

    Chrome driver

--- Library/ChromeWebDriver.py
+++ Library/ChromeWebDriver.py
@@ -3,28 +3,28 @@ import webdriver_manager
 from webdriver_manager.chrome import ChromeDriverManager
 import zipfile
 import os
-# from msedge.selenium_tools import Edge,EdgeOptions
+from msedge.selenium_tools import Edge,EdgeOptions
 
 def set_chromedriver():
 
     #webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24").install())
-    driver = webdriver.Chrome(executable_path=r'C:\Users\neogis\D Drive\FREEDOM\Python\STL_Automation\venv\WebDriverManager\chrome\90.0.4430.24\chromedriver_win32\chromedriver.exe')
+    driver = webdriver.Chrome(executable_path=r'C:\Users\neogis\Downloads\chromedriver_win32 (2)\chromedriver.exe')
     driver.get('http://google.com/')
     #driver = webdriver.Chrome(r"C:\Users\\scmselenium\\.wdm\\drivers\\chromedriver\\win32\\90.0.4430.24\\chromedriver.exe")
 
-    #app_path = r"C:\Users\scmselenium\ChromeDriver\chromedriver.exe"
-    #os.environ["PATH"] += os.pathsep + app_path
-    #result=os.environ['PATH']
-    #return result
+    app_path = r"C:\Users\neogis\Downloads\chromedriver_win32 (2)\chromedriver.exe"
+    os.environ["PATH"] += os.pathsep + app_path
+    result=os.environ['PATH']
+    return result
 
-# def set_edgedriver():
-#     options=EdgeOptions()
-#     options.use_chromium=True
-#     driver=Edge(options=options,executable_path=r"C:\Users\scmselenium\jenkins\workspace\print-management-ui\CPM_ProdValidation_Pipeline_US@2\venv\Lib\site-packages\MicrosoftWebDriver.exe")
-#     driver.get('http://google.com/')
-#     app_path = r"C:\Users\scmselenium\jenkins\workspace\print-management-ui\CPM_ProdValidation_Pipeline_US@2\venv\Lib\site-packages\MicrosoftWebDriver.exe"
-#     os.environ["PATH"] += os.pathsep + app_path
-#     result=os.environ['PATH']
-#     return result
+def set_edgedriver():
+    options=EdgeOptions()
+    options.use_chromium=True
+    driver=Edge(options=options,executable_path=r"C:\Users\scmselenium\jenkins\workspace\print-management-ui\CPM_ProdValidation_Pipeline_US@2\venv\Lib\site-packages\MicrosoftWebDriver.exe")
+    driver.get('http://google.com/')
+    app_path = r"C:\Users\scmselenium\jenkins\workspace\print-management-ui\CPM_ProdValidation_Pipeline_US@2\venv\Lib\site-packages\MicrosoftWebDriver.exe"
+    os.environ["PATH"] += os.pathsep + app_path
+    result=os.environ['PATH']
+    return result
 
-
+set_chromedriver()
